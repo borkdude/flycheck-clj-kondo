d8a6ee9a16aa24b5be01f1edf9843d41bdc75555
ikappaki <34983288+ikappaki@users.noreply.github.com>
2021-12-27 22:26:14 +0000

    Set the data encoding to/from clj-kondo to UTF-8 (#17)
    * Add test suite, integrate with Eldev and Github CI
    
    * Set UTF-8 encoding for data communicated to and from clj-kondo.
    
    Co-authored-by: ikappaki <ikappaki@users.noreply.github.com>

---
+++ test/corpus/utf8.clj
@@ -0,0 +1,4 @@
+(ns test.corpus.utf8)
+
+;; should be able to parse UTF-8 ok.
+(def two 'δύο)
---
+++ test/flycheck-clj-kondo-test.el
@@ -0,0 +1,55 @@
+(require 'flycheck-clj-kondo)
+(require 'flycheck-ert)
+
+(ert-deftest clj-kondo-find ()
+    (should (executable-find "clj-kondo")))
+
+(flycheck-ert-def-checker-test clj-kondo-clj clj basic
+  (flycheck-ert-should-syntax-check
+   "test/corpus/basic.clj" 'clojure-mode
+   '(3 23 warning "unused binding y"
+       :checker clj-kondo-clj)
+   '(3 25 warning "unused binding z"
+       :checker clj-kondo-clj)
+   '(4 65 warning "unused binding y"
+       :checker clj-kondo-clj)
+   '(5 25 warning "unused binding y"
+       :checker clj-kondo-clj)
+   '(5 29 warning "unused binding zs"
+       :checker clj-kondo-clj)
+   '(7 1 error "corpus.basic/public-fixed is called with 1 arg but expects 3"
+       :checker clj-kondo-clj)
+   '(10 1 error "corpus.basic/public-multi-arity is called with 3 args but expects 1 or 2"
+        :checker clj-kondo-clj)
+   '(11 1 error "corpus.basic/public-varargs is called with 1 arg but expects 2 or more"
+       :checker clj-kondo-clj)))
+
+(flycheck-ert-def-checker-test clj-kondo-cljc cljc basic
+  (flycheck-ert-should-syntax-check
+   "test/corpus/basic.cljc" 'clojurec-mode
+   '(3 26 warning "unused binding y"
+       :checker clj-kondo-cljc)
+   '(13 9 error "test.corpus.basic/foo is called with 1 arg but expects 2"
+        :checker clj-kondo-cljc)
+   '(14 10 error "test.corpus.basic/foo is called with 2 args but expects 1"
+        :checker clj-kondo-cljc)
+   '(21 1 error "test.corpus.basic/bar is called with 3 args but expects 1"
+        :checker clj-kondo-cljc)))
+
+(flycheck-ert-def-checker-test clj-kondo-cljs cljs basic
+  (flycheck-ert-should-syntax-check
+   "test/corpus/basic.cljs" 'clojurescript-mode
+   '(11 3 warning "use :else as the catch-all test expression in cond"
+        :checker clj-kondo-cljs)
+   '(18 3 warning "use :else as the catch-all test expression in cond"
+        :checker clj-kondo-cljs)))
+
+(flycheck-ert-def-checker-test clj-kondo-edn edn basic
+  (flycheck-ert-should-syntax-check
+   "test/corpus/basic.edn" 'clojure-mode
+   '(1 10 error "duplicate key a"
+        :checker clj-kondo-edn)))
+
+(flycheck-ert-def-checker-test clj-kondo-clj clj utf8
+  (flycheck-ert-should-syntax-check
+   "test/corpus/utf8.clj" 'clojure-mode))
